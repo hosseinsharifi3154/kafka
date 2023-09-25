057dd53d8527b25ae0d20f4de339fd0d4f4160ce
hosseinsharifi3154 <hosseinsharifi3154@gmail.com>
2023-09-25 13:40:21 +0330

    big commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+var consumerConfig = new ConsumerConfig
+
+{
+
+    BootstrapServers = "192.168.18.128",
+
+    GroupId = "group2",
+
+    AutoOffsetReset = AutoOffsetReset.Latest
+
+};
+var key = "samplekey1234567";
+using var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+TopicPartitionOffset topicPartitionOffset = new TopicPartitionOffset("sampleTopic1010", new Partition(1), Offset.Beginning);
+consumer.Assign(new List<TopicPartitionOffset> { topicPartitionOffset });
+try
+
+{
+
+    while (true)
+
+    {
+
+        var result = consumer.Consume(TimeSpan.FromSeconds(1));
+        if (result != null)
+        {
+            string decryptedMessage = Decrypt(result.Message.Value, key);
+            Console.WriteLine($"Consumed message '{decryptedMessage}' at: '{result.TopicPartitionOffset}'.");
+
+        }
+
+        if (result == null)
+
+        {
+
+            continue;
+
+        }
+
+    }
+
+}
+
+catch (Exception ex)
+
+{
+
+    Console.WriteLine($"Error: {ex.Message}");
+
+}
+
+static string Decrypt(string encryptedMessage, string key)
+{
+    using (Aes aes = Aes.Create())
+    {
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = new byte[16];
+
+        ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+        byte[] encryptedBytes = Convert.FromBase64String(encryptedMessage);
+        byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+        return Encoding.UTF8.GetString(decryptedBytes);
+    }
+}
---
+++ Kafka_producer/Kafka_producer/Program.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using System.Security.Cryptography;
+using System.Text;
+
+var config = new ProducerConfig { 
+    BootstrapServers = "192.168.18.128",
+};
+var key = "samplekey1234567";
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+TopicPartition topicPartition = new TopicPartition("sampleTopic1010", new Partition(1));
+Console.WriteLine("Enter the input pls :   ");
+bool exit = true;
+while (exit)
+{
+    string value = Console.ReadLine();
+    if (value == "q")
+    {
+        exit = false;
+    }
+    else
+    {
+        string encryptedMessage = Encrypt(value, key);
+        await producer.ProduceAsync(topicPartition, new Message<Null, string> { Value = encryptedMessage });
+        producer.Flush(TimeSpan.FromSeconds(10));
+
+    }
+}
+static string Encrypt(string message, string key)
+{
+    using (Aes aes = Aes.Create())
+    {
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = new byte[16];
+
+        ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+        byte[] encryptedBytes = encryptor.TransformFinalBlock(Encoding.UTF8.GetBytes(message), 0, message.Length);
+        return Convert.ToBase64String(encryptedBytes);
+    }
+}
+Console.ReadLine();
